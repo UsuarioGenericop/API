09ab913c5df014ed9862e21ab4a0bfc40f15d96f
Andres <andydquintero@gmail.com>
2025-11-04 23:02:30 -0500

    SendResponse

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ app.get('/tshirt', (req, res) => {
 res.status(200).send({
 tshirt: 'tshirt', size: 'large'})
 });
+
 app.post('/tshirt/:id', (req, res) => {
 const {id} = req.params;
 const {logo} = req.body;
 
-if (!logo){res.status(418).send({message: 'Logo needed'})
+    if (!logo){
+        res.status(418).send({message: 'Logo needed'})
+    }
+
+    res.send({
+        tshirt: `tshirt with your ${logo} and ID of ${id}`),
+    });
 });
